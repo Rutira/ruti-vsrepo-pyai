cc53bc05f34b07521b1fca3516e3e732d4a4c28a
Rutira <48622565+Rutira@users.noreply.github.com>
2025-08-18 20:46:48 +0300

    all

---
+++ rutiai-yolo/rutiai_yolo.py
@@ -0,0 +1,10 @@
+from unittest import result
+import pip
+name = "Bob"
+age =30 
+
+# readbooks_one # type: ignore
+
+def readbooks_one():
+    print(f"Привет, меня зовут {name} и мне {age} лет.")
+    result = sout
